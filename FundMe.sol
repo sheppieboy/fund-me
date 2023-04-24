e8c62b64c0baa47ce390ad3931c12d89c045178b
Luke Sheppard <sheppiegit@gmail.com>
2023-04-24 17:27:37 -0400

    Added chainlink price aggregator interface for sepoelia testnet.

--- FundMe.sol
+++ FundMe.sol
@@ -5,6 +5,35 @@ pragma solidity ^0.8.7;
 //Withdraw fund
 //Set minimum funding value in USD
 
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
 contract FundMe {
 
     uint256 public min_usd = 50;
@@ -16,7 +45,8 @@ contract FundMe {
     }
 
     function getPrice() public{
-        
+        //ABI
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
 
     }
 
