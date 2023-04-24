8ebbf319bd3dde361c35f730e3d360a31248f4be
Luke Sheppard <sheppiegit@gmail.com>
2023-04-24 17:44:42 -0400

    Added a funders array of addresses and a mapping to store the address of each funder and how much they sent.

--- FundMe.sol
+++ FundMe.sol
@@ -39,10 +39,16 @@ contract FundMe {
 
     uint256 public min_usd = 50 * 1e18;
 
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+
     function fund() public payable {
         //want to be able to set a mininium fund amount in USD
         //1. How do we send ETH to this contract
         require(getConversionRate(msg.value) >= min_usd, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
     function getPrice() public view returns (uint256){
